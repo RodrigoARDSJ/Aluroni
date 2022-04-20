46c51a72db0198674cf48ed22064543294c1080d
Rodrigo Resende <rresende@magnasistemas.com.br>
2022-04-20 15:55:23 -0300

    Projeto de estudos de React

---
+++ src/pages/carrinho/Cardapio/index.tsx
@@ -0,0 +1,40 @@
+import "normalize.css";
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import { useState } from "react";
+import Ordenador from "../ordenador";
+import Buscador from "../buscador";
+import Filtros from "../filtros";
+
+export default function Cardapio() {
+
+    const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
+
+    return (
+        <main>
+            <nav className={styles.menu}>
+                <Logo />
+                alurobin
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
+
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador
+                    busca={busca}
+                    setBusca={setBusca} />
+
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro} />
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
+                </div>
+            </section>
+        </main>
+    )
+}
